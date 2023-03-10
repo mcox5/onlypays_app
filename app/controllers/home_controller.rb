a3e3969d11be0a8179c58796f205a83c61d1bfe0
Matias Cox <mcox5@uc.cl>
2023-03-10 14:56:40 -0300

    Ventas por categoría listo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,18 @@
 class HomeController < ApplicationController
   def index
-    @sales_by_category = Category.joins(products: :sales)
-                                 .group("categories.id")
-                                 .select("categories.name, count(sales.id) as number_of_sales")
+    @sales_per_category = sales_per_category
+  end
+
+  private
+
+  # Vamos a recorrer las ventas por categoría de manera que nos retorne un hash con los resultados
+  def sales_per_category
+    results = {}
+    Category.all.each do |category|
+      total_sales = category.sales_product.size
+      total_quantity = category.sales_product.sum(&:quantity)
+      results[category] = { :total_sales => total_sales, :total_quantity => total_quantity}
+    end
+    return results
   end
 end
