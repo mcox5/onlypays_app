1e3a808334772b058324ee4e4ba47c91cc3f3dfc
Matias Cox <mcox5@uc.cl>
2023-03-12 13:30:04 -0300

    cambios en el gem file, no puedo solucionar problema de DELETE cuando hago un rails test, pequeños cambios en HTML

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -4,4 +4,8 @@ class ClientTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should have 10 clients" do
+    number_of_clients = Client.count
+    assert_equal 10, number_of_clients, "Expected 10 clients in the database"
+  end
 end
